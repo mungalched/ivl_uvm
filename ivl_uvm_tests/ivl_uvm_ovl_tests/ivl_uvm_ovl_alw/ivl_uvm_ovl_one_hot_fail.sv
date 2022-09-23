73e7c59682a3f447687f0682cd63b47fcff9b9e6
mungalched <91460700+mungalched@users.noreply.github.com>
2022-09-23 14:01:19 +0530

    added pass&fail tests for one_hot checker

---
+++ ivl_uvm_tests/ivl_uvm_ovl_tests/ivl_uvm_ovl_alw/ivl_uvm_ovl_one_hot_fail.sv
@@ -0,0 +1,53 @@
+module test;
+
+   wire clk;
+   reg rst_n;
+   reg[3:0]data;
+   initial begin
+      // Dump waves
+      $dumpfile("dump.vcd");
+      $dumpvars(1, test);
+   end    
+   // simple signal check OVL 
+   ovl_zero_one_hot #( .width(4)) u_ovl_zero_one_hot ( 
+	  
+			     .clock     (clk),
+			     .reset     (rst_n), 
+			     .enable    (1'b1),
+			     .test_expr (data)
+			     );
+   
+
+   initial begin
+   
+      // Initialize values.
+      rst_n = 1;
+      data = 4'b0000;
+      wait_clks(1);
+      $display("ovl_zero_one_hot does not fire at rst_n");
+      rst_n = 0;
+      $display("---ovl_one_hot checker fired---");
+      data =4'b1100;
+      $display("ovl_one_hot has fired: test_expr value is :%d",data); 
+      wait_clks(3);
+      data = 4'b0111;
+      $display("ovl_one_hot has fired: test_expr value is :%d",data);
+
+      wait_clks(6);
+      data = 4'b0x01;
+      $display("ovl_one_hot has fired: test_expr value is :%d",data);
+
+      wait_clks(5);
+      data = 4'b1111;
+      $display("ovl_one_hot has fired: tesr_expr value is :%d",data);
+      wait_clks(7);
+      $finish;
+   end
+
+   task wait_clks (input int num_clks = 1);
+      repeat (num_clks) @(posedge clk);
+   endtask : wait_clks
+
+  ivl_uvm_ovl_clk_gen #(.FREQ_IN_MHZ(100)) u_clk_100 (clk);
+
+endmodule
